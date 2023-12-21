0961dc4f3e64bf31c8f64ff131fa6977643b2421
MariaKundrat <03032005km@gmail.com>
2023-12-21 19:44:18 +0200

    Add basic structure and header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.scss';
+import logo from "../../images/logo.png";
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <nav>
+                <ul className='nav_ul'>
+                    <li>
+                        <img className='logo' src={logo} alt='логотип' />
+                    </li>
+                    <li>Головна</li>
+                    <li>Мій аккаунт</li>
+                    <li>Налаштування</li>
+                    <li>Про нас</li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
